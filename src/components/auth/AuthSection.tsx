460d4ee800ffc506ac0e8fe4e9c9185a8530ab68
Pavel Karpovich <Chesh397@mail.ru>
2019-11-21 18:23:31 +0300

    New SVG logo

--- src/components/auth/AuthSection.tsx
+++ src/components/auth/AuthSection.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import AuthMethods from './AuthMethods';
 import styles from './AuthSection.module.scss';
+import { ReactComponent as LogoSvg } from 'assets/chats.svg';
 
 const AuthSection: React.FC = function() {
   return (
-    <Container fluid className={'h-100 d-flex align-items-center justify-content-center ' + styles.authSec}>
+    <Container fluid
+      className={'h-100 d-flex flex-column align-items-center justify-content-center ' + styles.authSec}
+    >
+      <Row>
+        <LogoSvg width="300px" />
+      </Row>
+      <Row>
+        <h2>LogIn:</h2>
+      </Row>
       <Row className="w-100">
         <AuthMethods></AuthMethods>
       </Row>
