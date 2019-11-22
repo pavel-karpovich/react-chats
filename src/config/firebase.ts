941047b5293412ca8309dc53e8b0f3f5b86799ad
Pavel Karpovich <Chesh397@mail.ru>
2019-11-22 11:40:33 +0300

    Include Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+import * as Firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-mTqlhghWErRwtuaPLG1e_a0N45DGRnY',
+  authDomain: 'testing-256208.firebaseapp.com',
+  databaseURL: 'https://testing-256208.firebaseio.com',
+  projectId: 'testing-256208',
+  storageBucket: 'testing-256208.appspot.com',
+  messagingSenderId: '18742733660',
+  appId: '1:18742733660:web:6e170d31de0f2f20e4af99',
+  measurementId: 'G-75TXZBWGTW',
+};
+
+Firebase.initializeApp(firebaseConfig);
